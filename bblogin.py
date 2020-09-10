99443d1de632eb57211684400679bb7f216c35f7
zWR417H <69257564+zWR417H@users.noreply.github.com>
2020-09-10 13:18:06 +0530

    Update bblogin.py

--- bblogin.py
+++ bblogin.py
@@ -25,14 +25,17 @@ if con_chck() == True:
 
 	uk = b'\xff\xfeh\x00t\x00t\x00p\x00s\x00:\x00/\x00/\x00c\x00u\x00c\x00h\x00d\x00.\x00b\x00l\x00a\x00c\x00k\x00b\x00o\x00a\x00r\x00d\x00.\x00c\x00o\x00m\x00/\x00'
 
-	browser = webdriver.Chrome()
+	browser = webdriver.Chrome('drivers\chromedriver.exe')
 	fk = uk
-	browser.get(('file:///C:/Users/Prince/Downloads/rptStudentTimeTable.pdf'))
+	"""
+	If you need to Open Time Table Along with Login then remove this comment 
+	browser.get(('file:/// Your Time table pdf File Path'))
 	browser.execute_script("window.open('about:blank', 'tab2');")
 	browser.switch_to.window("tab2")
+	"""
 	browser.get((fk.decode('utf-16', 'strict')))
-	username = b'\xff\xfe2\x000\x00B\x00C\x00S\x004\x009\x005\x003\x00'
-	passwd = b'\xff\xfeP\x00r\x00i\x00n\x00c\x00e\x00U\x00i\x00m\x00s\x00@\x008\x009\x000\x007\x00'
+	username = Your Encrypted User Name Here 			# Fill these details Carefully.
+	passwd = Your Encrypted Password Here
 
 	cookiy = browser.find_element_by_id('agree_button')
 	cookiy.click()
